de5dc927ffd91cf962a767ac0e505beb8a2aaa7f
edo9924 <73080140+edo9924@users.noreply.github.com>
2021-11-11 10:45:23 -0400

    cambios2

--- datoscompraalumno.php
+++ datoscompraalumno.php
@@ -33,14 +33,23 @@
       text-align: center;
     }
 
+    .centrardiv{
+      margin: auto;
+      width: 60%;
+    }
+
 </style>
 </head>
-<body>
+<body>    
 	<!-- Bootstrap JavaScript -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    <script>
+    function myFunction(){
+      location.href = 'detallecurso.php';
+    }
+    </script>
 	<!-- NAVBBAR -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="index.php">LosK-Tutos</a>
@@ -84,12 +93,43 @@
     <a class="nav-link" aria-current="page" href="panelalumno.php">Perfil</a>
     <br>
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="datoscompraalumno.php">Datos de compra</a>
+    <a class="nav-link active" aria-current="page" href="datoscompraalumno.php">Tus Cursos</a>
   </li>
   </ul>
   <br>
 
      <!-- FIN PANEL -->
+     <div class="centrardiv">  
+  <h2>Nombre Curso</h2>
+  <table border="1" onclick="myFunction()">
+    <tr>    
+      <td rowspan="2" style="vertical-align: top;"><img src="img/lobosthumb.jpg" width="300px"></td>
+      <td style="vertical-align: top; width: 500px;"><a>En este Lugar ira una breve descripcion del curso que se esta impartiendo</a>
+        <tr>
+          <td>
+            <a>Fecha en que se compro:</a>
+          </td>
+        </tr>
+      </td>
+    </tr>
+    
+  </table>
+  <h2>Nombre Curso</h2>
+  <table border="1" onclick="myFunction()">
+    <tr>
+      <td rowspan="2" style="vertical-align: top;"><img src="img/pelaothumb.jpg" width="300px"></td>
+      <td style="vertical-align: top; width: 500px;"><a>En este Lugar ira una breve descripcion del curso que se esta impartiendo</a></td>
+      <tr>
+          <td>
+            <a>Fecha en que se compro:</a>
+          </td>
+        </tr>
+      </td>
+    </tr>
+    </tr>
+  </table>
+</div>
+<br>
 
   <!-- Footer -->
   <section class="">
